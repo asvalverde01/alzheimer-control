051a03e835b01a6f711857feffb5971a04fe1cd8
Alberto Valverde <asvalverde01@gmail.com>
2021-12-20 18:27:13 -0500

    Inicio Proyecto
    inicio

--- src/app/logic/Main.java
+++ src/app/logic/Main.java
@@ -3,28 +3,5 @@ package app.logic;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-
-        System.out.println("Thyara estuvo aquí");
-		System.out.println("SEGUNDO INTENTO");
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("VACACIONES!!!");
-=======
-<<<<<<< HEAD
-        System.out.println("Sebastian también estuvo aquí");
-=======
-        System.out.println("A nadie le importa <3");
->>>>>>> 9a44802070b1ee690ffd543dd840233457f270c1
->>>>>>> f15d3558ef650ba563ece3ab065e8716bc464954
-=======
-
-        System.out.println("Tengo sueño");
-
-        System.out.println("Sebastian también estuvo aquí");
-
-        System.out.println("A nadie le importa <3");
-
->>>>>>> ac70dada8a07b817c9746921f878ffdf63762d0d
     }
 }
